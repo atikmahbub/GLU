1c2fd36db51abcfbe5dbb6cda48b2c776e73c74e
unknown <gyanendra@antino.io>
2020-06-09 15:17:46 +0530

    updated the accountant page

--- src/Views/AddNewAccountant/FormRow.tsx
+++ src/Views/AddNewAccountant/FormRow.tsx
@@ -6,13 +6,13 @@ import AdornmentInput from '../../components/Inputs/AdornmentInput';
 const FormRow = () => {
     return (
         <div className="row">
-            <div className="col-lg-4">
+            <div className="col-lg">
                 <TextField className="custom-input" label="Name" variant="outlined" fullWidth />
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg">
                 <TextField className="custom-input" label="Email" variant="outlined" fullWidth />
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg">
                 <SelectField
                     className="custom-input"
                     options={['Male', 'Female']}
@@ -20,16 +20,10 @@ const FormRow = () => {
                     getValue={() => {}}
                 />
             </div>
-            <div className="col-lg-3">
-                <SelectField className="custom-input" options={['computer', 'English', 'science']} label="Department" getValue={() => {}} />
-            </div>
-            <div className="col-lg-3">
-                <SelectField className="custom-input" options={['senior lecturer', 'junior lecturer']} label="Designation" getValue={() => {}} />
-            </div>
-            <div className="col-lg-3">
+            <div className="col-lg">
                 <TextField className="custom-input" label="Phone Number" variant="outlined" fullWidth />
             </div>
-            <div className="col-lg-3">
+            <div className="col-lg">
                 <AdornmentInput className="custom-input mb-4" />
             </div>
         </div>
