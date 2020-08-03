5a892348f7c13838d6ebefe9b879da9186142462
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-08-03 13:34:42 +0530

    Add about comtainer

--- src/Views/TutorDetail/About.tsx
+++ src/Views/TutorDetail/About.tsx
@@ -3,15 +3,27 @@ import SelectFieldUnderline from '../../components/Inputs/SelectFieldUnderline';
 import DateSelector from '../../components/DateTimeSelector/DateSelector';
 
 import { Button } from '@material-ui/core';
+import Skill from './Skill';
 const About: React.FunctionComponent = () => {
     return (
         <div className="about__container">
             <h1 className="heading">About</h1>
             <div className="description__box">
-                <p>
+                <p className="description">
                     I am an American author, life coach, and philanthropist. Known for my infomercials, seminars, and
                     self-help books including the books Unlimited Power and...
                 </p>
+                <h2 className="skill">Skills</h2>
+                <div className="skill__box">
+                    <Skill title="Mathematics" />
+                    <Skill title="English" />
+                    <Skill title="Hindi" />
+                    <Skill title="Computer Science" />
+                    <Skill title="Project Management" />
+                    <Skill title="Japanese" />
+                    <Skill title="Japanese" />
+                    <Skill title="Japanese" />
+                </div>
             </div>
         </div>
     );
