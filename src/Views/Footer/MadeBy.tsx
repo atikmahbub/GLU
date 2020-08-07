00fb6b08dba08d9344a67b4ed16a163c34695585
Shashank Yadav <shashank.5060@gmail.com>
2020-08-08 03:26:03 +0530

    homepage minor ui changes

--- src/Views/Footer/MadeBy.tsx
+++ src/Views/Footer/MadeBy.tsx
@@ -4,17 +4,23 @@ import { Typography } from '@material-ui/core';
 const MadeBy = () => {
     return (
         // <div>
-            <div className="row">
-                <div className="col-9">
-                    <Typography className="build-by">
-                        Made &nbsp;by&nbsp; <span><a href="https://madebysix.com/">Six</a></span> &nbsp;&nbsp;&nbsp; Build&nbsp; by&nbsp;
-                        <span><a href="https://www.antino.io/">Antino Labs</a></span>
-                    </Typography>
-                </div>
-                <div className="col-3">
-                    <Typography className="glu">Glu 2020</Typography>
-                </div>
+        <div className="row" style={{ marginBottom: '2rem' }}>
+            <div className="col-9">
+                <Typography className="build-by">
+                    Made by{' '}
+                    <span>
+                        <a href="https://madebysix.com/">Six</a>
+                    </span>{' '}
+                    &nbsp;&nbsp; Build by&nbsp;
+                    <span>
+                        <a href="https://www.antino.io/">Antino Labs</a>
+                    </span>
+                </Typography>
             </div>
+            <div className="col-3">
+                <Typography className="glu">Glu 2020</Typography>
+            </div>
+        </div>
         // </div>
     );
 };
