187534a5989174b0010c09ca0020c3d624fd9d6d
unknown <gyanendra@antino.io>
2020-08-10 13:14:21 +0530

    made the watch previous class video player

--- src/Views/StudentWatchPreviousClass/VideoPlayerContainer.tsx
+++ src/Views/StudentWatchPreviousClass/VideoPlayerContainer.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import ReactAwesomePlayer from 'react-awesome-player';
+const ReactPlayer =  React.lazy(()=>import('react-player'));
 
 const VideoPlayerContainer = () => {
     return (
-        <div>
-            
+        <div className="video__player">
+            <ReactPlayer className="video" url="https://youtu.be/HhjHYkPQ8F0" controls={true}  width="100%" height="85vh" />
         </div>
     );
-}
+};
 
 export default VideoPlayerContainer;
