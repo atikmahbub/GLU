5eefba7b9bd124d9631541d003c14f596a024258
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-08-18 12:31:53 +0530

    Add student incomplete detail page

--- src/Views/StudentDashboard/StudentIncompleteDetail/index.tsx
+++ src/Views/StudentDashboard/StudentIncompleteDetail/index.tsx
@@ -9,19 +9,13 @@ import commonImg from '../../../Assets/images';
 
 const Index: React.FunctionComponent = () => {
     return (
-        <div className="dashboard">
+        <div className="dashboard__Incomplete">
             <FixedNavigation>
                 <NavigationMenu menuList={menus} />
             </FixedNavigation>
             <DashboardWrapper>
                 <img src={commonImg.boyCropped} className="dashboard__wrapper__image" alt="boy" />
-                <Heading
-                    name="Frank Howard"
-                    about=" I’m Frank, a secondary Student at GEMS school in Dubai. Currently studying Maths, Business Studies
-                    and History."
-                    country=" Dubai, UAE"
-                    contact="(+971) 4 554 0350 frankhwrd@gmail.com"
-                />
+                <Heading name="Frank Howard" country=" Dubai, UAE" contact="(+971) 4 554 0350 " />
             </DashboardWrapper>
             <div className="footer">
                 <MadeBy />
