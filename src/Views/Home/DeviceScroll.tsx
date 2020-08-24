2d141dbfc03170cec03f04a532a968f4d3c3851b
unknown <gyanendra@antino.io>
2020-08-24 12:19:24 +0530

    change the device scroll px size

--- src/Views/Home/DeviceScroll.tsx
+++ src/Views/Home/DeviceScroll.tsx
@@ -12,8 +12,10 @@ const DeviceScroll: React.FunctionComponent<props> = ({ image, containerName }) 
             const deviceContainer = document.querySelector(`#${containerName}`);
             const bounding: any = deviceContainer?.getBoundingClientRect();
             const winwowHeight = window.innerHeight || document.documentElement.clientHeight;
-            if (winwowHeight - bounding.top >= 300 && bounding.bottom > 0) {
-                const scrollAt = bounding.top - winwowHeight + 300;
+            const a = winwowHeight - bounding.top;
+            console.log(bounding)
+            if (winwowHeight - bounding.top >= 400 && bounding.bottom > 0) {
+                let scrollAt = bounding.top - winwowHeight + 400;
                 (device as any).current.style.cssText = `will-change: transform;
                 transform: translate3d(0px, ${scrollAt}px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
                 transform-style: preserve-3d;`;
