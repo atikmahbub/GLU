0c8e59ca94357dd36e6bac1a24cd391e398bd13f
unknown <gyanendra@antino.io>
2020-08-27 13:24:13 +0530

    fix the font issue of the login and signup page

--- src/Routes/RootRoute/rootPrivateRoute.tsx
+++ src/Routes/RootRoute/rootPrivateRoute.tsx
@@ -2,5 +2,8 @@ import React from "react";
 const Dashboard = React.lazy(() => import('../../Views/Dashboard/index'));
 
 export const rootPrivateRoute = [
-    {component:Dashboard, name:'/dashboard/'}
+    {component:Dashboard, name:'/dashboard/'},
+    {component:Dashboard, name:'/student/'},
+    {component:Dashboard, name:'/parent/'},
+    {component:Dashboard, name:'/teacher/'},
 ]
--- src/Views/StudentLogin/StudentLogin.tsx
+++ src/Views/StudentLogin/StudentLogin.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavigationMenu from '../../components/NavigationMenu';
-import { TextField, Button, FormControlLabel, Radio, Typography } from '@material-ui/core';
+import { TextField, FormControlLabel, Radio, Typography } from '@material-ui/core';
 import UnderLineAddornment from '../../components/Inputs/UnderLineAddornment';
 import { useHistory } from 'react-router';
 import OutlineButton from '../../components/Button/OutlineButton';
 
 const StudentLogin = () => {
+    const [state, setState] = useState({email:'', password:''});
     const routes = useHistory();
     const handleForgot = () => {
         routes.push('/student-forgot-password');
     };
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        setState({...state, email:e.target.value});
+    }
+    const handlePass = (value: string)=>{
+        setState({...state, password: value});
+    }
+    console.log(state)
     return (
-        
         <div className="credential__container">
             <NavigationMenu/>
             <div className="credential__form__container">
                 <div className="form-container">
-                    <TextField className="line-input mb-5" label="Email Address" fullWidth />
-                    <UnderLineAddornment label="Password" className="custom-adornment-input"/>
+                    <TextField className="line-input mb-5" label="Email Address" value={state.email} onChange={handleEmail} fullWidth />
+                    <UnderLineAddornment label="Password" className="custom-adornment-input"  onChange={(value:string)=>handlePass(value)}/>
                     <div className="button-container">
-                    <OutlineButton text="Sign In"/>
+                    <OutlineButton text="Sign In" />
                     <FormControlLabel color="primary" value="" className="keep__signed radio-button" control={<Radio />} label="Keep me signed in" />
                     </div>
                 </div>
