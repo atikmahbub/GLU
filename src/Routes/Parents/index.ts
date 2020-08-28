1a68cb7a9f46b3994c82350ff1073d25da59001f
Rahul Kant <rahul.kant@gtpl.com>
2020-08-28 18:59:11 +0530

    dashboard parent

--- src/Routes/Parents/index.ts
+++ src/Routes/Parents/index.ts
@@ -2,7 +2,8 @@ import ParentsHomePage from '../../Views/ParentsHomePage/ParentsHome';
 import { createRouteObj } from '../../Helper/routes';
 
 const routes = [
-    createRouteObj('/parent-homepage', ParentsHomePage)
+    createRouteObj('/parent-homepage', ParentsHomePage),
+    createRouteObj('/parent/', ParentsHomePage)
 ];
 
 export default routes;
