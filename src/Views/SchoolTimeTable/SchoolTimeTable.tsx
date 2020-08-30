b6541522725d5d912ad2cb628d90645f3c65c00b
mridulk <mridulkh0302@gmail.com>
2020-08-30 22:38:20 +0530

    made school time-table component

--- src/Views/SchoolTimeTable/SchoolTimeTable.tsx
+++ src/Views/SchoolTimeTable/SchoolTimeTable.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProgressBar from '../PersonalSchool2/ProgressBar';
 import NavigationMenu from '../../components/NavigationMenu';
 import { Typography } from '@material-ui/core';
-
+import MadeBy from '../Footer/MadeBy';
 const PersonalSchoolToggle: React.FunctionComponent = () => {
     const menu = [
         { link: '/tutor/', name: 'Dashboard' },
@@ -15,10 +15,101 @@ const PersonalSchoolToggle: React.FunctionComponent = () => {
             <NavigationMenu menuList={menu} />
             <div className="personal_school_timetable_subcontainer">
                 <div className="row">
-                    <div className="col-md-6 p-0">
-                        <div className="personal_school_calendarDate">
-                            <Typography className="calendarsubText1">11th August 2020</Typography>
-                            <Typography className="calendarsubText2">9.45am</Typography>
+                    <div className="col-md-6">
+                        <div className="school_table_calendar_container">
+                        <div className="personal_school_calendarDate row">
+                            <div className="col-6 p-0">
+
+                                <Typography className="calendarsubText1">11th August 2020</Typography>
+                                <div className="school_calendar_component">
+                                    
+
+                                    <ul className="weekdays">
+                                        <li>Mo</li>
+                                        <li>Tu</li>
+                                        <li>We</li>
+                                        <li>Th</li>
+                                        <li>Fr</li>
+                                        <li>Sa</li>
+                                        <li>Su</li>
+                                    </ul>
+
+                                    <ul className="days">
+                                        <li>1</li>
+                                        <li>2</li>
+                                        <li>3</li>
+                                        <li>4</li>
+                                        <li>5</li>
+                                        <li>6</li>
+                                        <li>7</li>
+                                        <li>8</li>
+                                        <li>9</li>
+                                        <li>
+                                            <span className="active">10</span>
+                                        </li>
+                                        <li>11</li>
+                                        <li>12</li>
+                                        <li>13</li>
+                                        <li>14</li>
+                                        <li>15</li>
+                                        <li>16</li>
+                                        <li>17</li>
+                                        <li>18</li>
+                                        <li>19</li>
+                                        <li>20</li>
+                                        <li>21</li>
+                                        <li>22</li>
+                                        <li>23</li>
+                                        <li>24</li>
+                                        <li>25</li>
+                                        <li>26</li>
+                                        <li>27</li>
+                                        <li>28</li>
+                                        <li>29</li>
+                                        <li>30</li>
+                                        <li>31</li>
+                                        <li>
+                                        <span className="inActive">1</span>
+                                        </li>
+                                        
+                                        <li>
+                                        <span className="inActive">2</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">3</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">4</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">5</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">6</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">7</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">8</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">9</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">10</span>
+                                        </li>
+                                        <li>
+                                        <span className="inActive">11</span>
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <Typography className="calendarsubText2">9.45am</Typography>
+                            </div>
+                        </div>
                         </div>
                     </div>
                     <div className="col-md-6 school_borderline p-0">
@@ -28,7 +119,7 @@ const PersonalSchoolToggle: React.FunctionComponent = () => {
                                     <Typography className="calendarText">11th August 2020</Typography>
                                 </div>
                             </div>
-                            
+
                             <ProgressBar
                                 time={'8:30-9:25am'}
                                 subject={'Maths'}
@@ -80,6 +171,11 @@ const PersonalSchoolToggle: React.FunctionComponent = () => {
                             />
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="profile_footer">
+                <div className="footer">
+                    <MadeBy />
                 </div>
             </div>
         </div>
