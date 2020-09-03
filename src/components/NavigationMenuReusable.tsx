ac6eb0bb142c1bf10abc73f3544325f907176a2d
vivekregmi <vivek.r@antino.io>
2020-09-03 11:07:35 +0530

    design fixes

--- src/components/NavigationMenuReusable.tsx
+++ src/components/NavigationMenuReusable.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Button, Typography, IconButton, Grid } from '@material-ui/core';
 import { v4 as uuidv4 } from 'uuid';
 import { Search, Menu } from '@material-ui/icons';
-import {BigMenu} from './BigMenu'
+import { BigMenu } from './BigMenu';
 import commonImg from '../Assets/images';
 
 export interface propsType {
@@ -14,64 +14,69 @@ export interface propsType {
 interface props {
     menuList?: propsType[];
     customClass?: string;
+    customBackgroundColor?: string;
     handler?: () => void;
     showMenuOptions?: boolean;
 }
-const NavigationMenuReusable: React.FunctionComponent<props> = ({ menuList, handler, customClass, showMenuOptions }) => {
+const NavigationMenuReusable: React.FunctionComponent<props> = ({
+    menuList,
+    handler,
+    customClass,
+    showMenuOptions,
+    customBackgroundColor,
+}) => {
     const getMenuList = () => {
         if (menuList) {
             return (
                 <>
-                <li>
-                    <IconButton className="icon-button" onClick={handler}>
-                        <Menu className="icon" />
-                    </IconButton>
-                </li>
-                <li>
-                    <IconButton className="icon-button">
-                        <Search className="icon" />
-                    </IconButton>
-                </li>
+                    <li>
+                        <IconButton className="icon-button" onClick={handler}>
+                            <Menu className="icon" />
+                        </IconButton>
+                    </li>
+                    <li>
+                        <IconButton className="icon-button">
+                            <Search className="icon" />
+                        </IconButton>
+                    </li>
                     {menuList.map((item: propsType) => (
-                <li key={uuidv4()}>
-                    <Link to={item.link}>
-                        <Button className="link">{item.name}</Button>
-                    </Link>
-                </li>
-            ))
-            }
+                        <li key={uuidv4()}>
+                            <Link to={item.link}>
+                                <Button className="link">{item.name}</Button>
+                            </Link>
+                        </li>
+                    ))}
                 </>
             );
         } else {
-        return (
-                    <>
-            <li>
-                <Link to="/signup">
-                    <Button className="outline-rec">Sign Up</Button>
-                </Link>
-            </li>
-            <li>
-                <Link to="/login">
-                    <Button>Sign In</Button>
-                </Link>
-            </li>
-            <li>
-                <Link to="/help-support">
-                    <Button>Help</Button>
-                </Link>
-            </li>
-                    </>
-                );
-    }
-        };
+            return (
+                <>
+                    <li>
+                        <Link to="/signup">
+                            <Button className="outline-rec">Sign Up</Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login">
+                            <Button>Sign In</Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/help-support">
+                            <Button>Help</Button>
+                        </Link>
+                    </li>
+                </>
+            );
+        }
+    };
     return (
         <div className="menu__type2__container">
-            <div className="navigation">
+            <div className={`navigation ${customBackgroundColor}`}>
                 <ul className={customClass}>{getMenuList()}</ul>
                 <Typography className={`heading ${customClass}`}>Glu</Typography>
             </div>
-            {showMenuOptions&&<BigMenu/>}
-            
+            {showMenuOptions && <BigMenu />}
         </div>
     );
 };
