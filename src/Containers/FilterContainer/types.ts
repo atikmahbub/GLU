dd6d62f8ca91d70b10149096cae18d3ca2260e01
Andriy Halushka <andrushagalushka@gmail.com>
2020-09-12 23:18:53 +0300

    modife FilterContainer component; add filter selection logic;

--- src/Containers/FilterContainer/types.ts
+++ src/Containers/FilterContainer/types.ts
@@ -1,4 +1,12 @@
+export type FilterValue = string | null;
+
 export interface IFilterElement {
     label: string;
+    value: FilterValue;
+}
+
+export interface IFilterDataElement {
+    label: string;
     value: string;
+    type: string;
 }
