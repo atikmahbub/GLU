a21af33b2fd254e9dc8b29ec63566ec4d69eb52e
unknown <gyanendra@antino.io>
2020-09-17 10:38:21 +0530

    pull from master and change the icon import name

--- src/Assets/Icons.ts
+++ src/Assets/Icons.ts
@@ -13,7 +13,7 @@ import googleWhite from './Icons/googleBlack.svg';
 import arrowDown from './Icons/arrowDown.svg';
 import calendar from './Icons/calendar.svg';
 import user from './Icons/user.svg';
-import exit from './Icons/Exit.svg';
+import exit from './Icons/exit.svg';
 
 export const Icons = {
     Facebook,
