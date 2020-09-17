a7fc18f87faeb2b6d4342320818fe2e148793859
Andriy Halushka <andrushagalushka@gmail.com>
2020-09-18 01:04:25 +0300

    fix UI students dashboard. create dashboard page container

--- src/Views/StudentsModule/Dashboard/index.tsx
+++ src/Views/StudentsModule/Dashboard/index.tsx
@@ -1,23 +1,5 @@
 import React, { FC } from 'react';
-// import Grid from '@material-ui/core/Grid';
-// import Tabs from './Tabs';
-// import School from './School';
-// import Personal from './Personal';
-// import StudentsPageContainer from '../StudentsPageContainer';
 import DashboardPageContainer from '../../../Containers/Pages/DashboardPageContainer';
-
-// const StudentsDashboard: FC = () => {
-//     const [activeTab, setActiveTab] = useState('personal');
-//     return (
-//         <StudentsPageContainer>
-//             <Grid container direction="column">
-//                 <Tabs value={activeTab} onChange={setActiveTab} />
-//                 {activeTab === 'school' && <School />}
-//                 {activeTab === 'personal' && <Personal />}
-//             </Grid>
-//         </StudentsPageContainer>
-//     );
-// };
 
 const StudentsDashboard: FC = () => {
     return (
