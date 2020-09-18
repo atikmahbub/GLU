6ba3e7f18b4945ead547fc6e0b983dc6f3249742
Rahul Kant <rahul.kant@gtpl.com>
2020-09-18 16:26:37 +0530

    improved Tutor Banner

--- src/components/ParentTutorIndividual/ParentIndividualTutorBanner.tsx
+++ src/components/ParentTutorIndividual/ParentIndividualTutorBanner.tsx
@@ -1,15 +1,13 @@
-import React, { FC, useState, useEffect, ReactNode, memo } from 'react';
+import React, { FC } from 'react';
 import { Typography } from '@material-ui/core';
 import commonImg from '../../Assets/images';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { FavoriteBorder, StarBorder } from '@material-ui/icons';
-// import './style.scss';
-import { createMuiTheme, responsiveFontSizes, MuiThemeProvider, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes, MuiThemeProvider } from '@material-ui/core/styles';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme)
@@ -19,7 +17,6 @@ const useStyles = makeStyles({
         position: 'relative',
         backgroundColor: '#5E5558',
         minWidth: '100%',
-        // minHeight: '1000px',
         border: '2px solid #5E5558',
         fontFamily: 'CircularXXWeb-Book',
         paddingBottom: "50px",
@@ -36,7 +33,6 @@ const useStyles = makeStyles({
         float: 'left',
         width: '33.437rem',
         height: '34.062rem',
-        // border: "2px solid red",
         [theme.breakpoints.down('sm')]: {
             marginTop: '10px',
             width: '70vw'
@@ -48,18 +44,22 @@ const useStyles = makeStyles({
         objectFit: 'cover',
     },
     title: {
-        marginLeft: '20px',
+        marginLeft: '3.125rem',
         marginTop: '14vh',
         color: 'white',
         fontSize: '2.625rem',
         minWidth: '100%'
     },
     titleColumn: {
+        padding: 0,
+        margin: 0,
         maxHeight: "200px",
     },
     secondHeader: {
+        padding: 0,
+        margin: 0,
         marginTop: '-10vh',
-        marginLeft: '20px',
+        marginLeft: '3.125rem',
         color: 'white',
         width: '100%',
         fontFamily: 'CircularXXWeb-Book',
@@ -91,15 +91,14 @@ const useStyles = makeStyles({
     favoriteText: {
         fontSize: '1.25rem',
     },
-    basicAdvance: {
+    bottomText: {
         display: "flex",
         alignItems: "center",
-        // width: "287px",
         padding: 0,
         color: 'white',
         position: 'absolute',
-        bottom: '5px',
-        left: '20px',
+        bottom: '2.125rem',
+        left: '3.125rem',
         fontSize: "1.562rem",
         marginTop: '146px',
     },
@@ -115,29 +114,16 @@ const useStyles = makeStyles({
 
 const ParentIndividualTutorBanner: FC = () => {
     const classes = useStyles();
-    const [height, setHeight] = useState(1000);
-    const updateWindowDimensions = () => {
-        let height = window.innerHeight;
-        setHeight(height);
-        //   setWidth(window.innerWidth);
-    }
-
-    useEffect(() => {
-        window.addEventListener('resize', updateWindowDimensions);
-    }, []);
-
     return (
         <MuiThemeProvider theme={theme}>
             <React.Fragment>
                 <div className={classes.body}>
-                    {/* <div style={{height}} className={classes.body}> */}
                     <Grid className={classes.gridContainer1} container spacing={3}>
                         <Grid className={classes.titleColumn} item xs={12} sm={12} md={6}>
                             <div className={classes.title}>Tutor</div>
                         </Grid>
                         <Grid item className={classes.imageGridContainer} xs={12} sm={12} md={6}>
                             <Container className={classes.imageContainer}>
-                                {/* <img className={classes.image} alt="" src={ImageFile}/> */}
                                 <LazyLoadImage
                                     alt=""
                                     effect="blur"
@@ -147,17 +133,15 @@ const ParentIndividualTutorBanner: FC = () => {
                         </Grid>
                     </Grid>
                     <div className={classes.secondHeader} >
-
                         <Typography className={classes.bigTitle}>Moly Pearce <br /> Dubai, UAE</Typography>
                         <div className={classes.iconBox}>
-
                             <div className={classes.favoriteBox}>
                                 <FavoriteBorder />
                                 <Typography style={{ marginLeft: '7px' }} className={classes.favoriteText}>Favorite</Typography>
                             </div>
                         </div>
                     </div>
-                    <div className={classes.basicAdvance}>
+                    <div className={classes.bottomText}>
                         <StarBorder />
                         <Typography className={classes.rating}>5/5 </Typography>
                         <Typography className={classes.primarySecondaryText}>Primary, Secondary </Typography>
