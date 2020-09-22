964a4065a7f76f899983a3b37b7c4264c761a2b4
Andriy Halushka <andrushagalushka@gmail.com>
2020-09-22 22:56:26 +0300

    add UI for tutor profile page container

--- src/Containers/CardsGridContainer.tsx
+++ src/Containers/CardsGridContainer.tsx
@@ -5,10 +5,11 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const useStyles = makeStyles({
     root: {
-        padding: ({ padding }: any) => padding ? '9.375rem 3.125rem' : 0,
+        padding: ({ padding }: any) => (padding ? '9.375rem 3.125rem' : 0),
         background: ({ background }: any) => (background === 'primary' ? '#fff' : '#F7F7F7'),
         paddingTop: ({ paddingTop, paddingTopVariant }: any) =>
             paddingTop ? (paddingTopVariant === 1 ? '9.375rem' : '6.25rem') : 0,
+        paddingBottom: ({ paddingBottomVariant }: any) => (paddingBottomVariant === 1 ? '9.375rem' : '6.25rem'),
     },
 });
 
@@ -18,6 +19,7 @@ interface ICardsGridContainer {
     padding?: boolean;
     paddingTop?: boolean;
     paddingTopVariant?: 1 | 2;
+    paddingBottomVariant?: 1 | 2;
 }
 
 const CardsGridContainer: FC<ICardsGridContainer> = ({
@@ -26,9 +28,10 @@ const CardsGridContainer: FC<ICardsGridContainer> = ({
     padding,
     paddingTop,
     paddingTopVariant,
+    paddingBottomVariant,
     children,
 }) => {
-    const classes = useStyles({ background, padding, paddingTop, paddingTopVariant });
+    const classes = useStyles({ background, padding, paddingTop, paddingTopVariant, paddingBottomVariant });
     return (
         <Grid container className={classNames(classes.root, rootClassName)}>
             {children}
@@ -41,6 +44,7 @@ CardsGridContainer.defaultProps = {
     padding: true,
     paddingTop: true,
     paddingTopVariant: 1,
+    paddingBottomVariant: 1,
 };
 
 export default CardsGridContainer;
--- src/Containers/Pages/TutorProfilePageContainer.tsx
+++ src/Containers/Pages/TutorProfilePageContainer.tsx
@@ -1,15 +1,29 @@
 import React, { FC, useMemo } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import NavigationMenu from '../../components/NavigationMenu';
 import { UserTypes } from '../../Types/user';
 import { createMenuList } from '../../Helper/menus';
 import LeftDrawerMenuContent from '../Menus/LeftDrawerMenuContent';
 import TutorProfileCard from '../../components/Cards/TutorProfileCard';
+import CardsGridContainer from '../CardsGridContainer';
+import CardsGrid from '../CardsGrid';
+import TitlePrimary from '../../components/Typographies/TitlePrimary';
+
+const useStyles = makeStyles({
+    limited: {
+        fontSize: '1.5625rem',
+        lineHeight: '1.5625rem',
+    },
+});
 
 interface ITutorProfilePageContainer {
     userType: UserTypes;
 }
 
 const TutorProfilePageContainer: FC<ITutorProfilePageContainer> = ({ userType }) => {
+    const classes = useStyles();
     const menuList = useMemo(() => createMenuList(userType), [userType]);
     return (
         <NavigationMenu
@@ -27,6 +41,22 @@ const TutorProfilePageContainer: FC<ITutorProfilePageContainer> = ({ userType })
                 description="Primary, Secondary"
                 img="https://res.cloudinary.com/ddwbbzuxw/image/upload/v1596608142/gluschool/smilegirl_ellmdm.jpg"
             />
+            <CardsGridContainer paddingTopVariant={2} paddingBottomVariant={2}>
+                <CardsGrid rows={2}>
+                    <Grid container>
+                        <Typography className={classes.limited}>Limited Availablitiy</Typography>
+                    </Grid>
+                    <Grid container>
+                        <Grid container direction="column">
+                            <TitlePrimary>
+                                I am an American author, life coach, and philanthropist. Known for my infomercials,
+                                seminars, and self-help books including the books Unlimited Power and Awaken the Giant
+                                Within. In 2015 and 2016 I was listed on the Worth Magazine Power 100 list.
+                            </TitlePrimary>
+                        </Grid>
+                    </Grid>
+                </CardsGrid>
+            </CardsGridContainer>
         </NavigationMenu>
     );
 };
