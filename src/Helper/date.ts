7536a383454d5e55d65b502f388627fdf777ff1a
Andriy Halushka <andrushagalushka@gmail.com>
2020-09-23 20:39:44 +0300

    refactor

--- src/Helper/date.ts
+++ src/Helper/date.ts
@@ -2,7 +2,7 @@ import parse from 'date-fns/parse';
 import format from 'date-fns/format';
 
 export function parseTime(time: string): string {
-    return format(parse(time.substr(0, 8), 'HH:mm:ss', new Date()), 'h:mma')l
+    return format(parse(time.substr(0, 8), 'HH:mm:ss', new Date()), 'h:mma');
 }
 
 export function getCurrentMonth(): string {
