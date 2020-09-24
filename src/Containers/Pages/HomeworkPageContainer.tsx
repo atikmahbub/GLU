163bd98b184c0ee56ca5e4e6443858b99df06e06
Andriy Halushka <andrushagalushka@gmail.com>
2020-09-24 15:53:23 +0300

    implement homework page container

--- src/Containers/Pages/HomeworkPageContainer.tsx
+++ src/Containers/Pages/HomeworkPageContainer.tsx
@@ -1,16 +1,47 @@
 import React, { FC, useMemo } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import NavigationMenu from '../../components/NavigationMenu';
 import LeftDrawerMenuContent from '../Menus/LeftDrawerMenuContent';
 import { UserTypes } from '../../Types/user';
 import { createMenuList } from '../../Helper/menus';
 import CardsGridContainer from '../CardsGridContainer';
+import ColumnsContainer from '../ColumnsContainer';
+import FilterContainer from '../FilterContainer';
+import HomeworkCard from '../../components/Cards/HomeworkCard';
+import TitlePrimary from '../../components/Typographies/TitlePrimary';
+import { classesFiltersData } from '../../data/filters';
+import { homeworks } from '../../data/homework';
+import PageFooter from '../../components/PageFooter';
+
+const useStyles = makeStyles({
+    filterContainer: {
+        marginBottom: '3.75rem',
+    },
+    counterTitle: {
+        fontSize: '1.25rem',
+        lineHeight: '1.5625rem'
+    },
+    counter: {
+        marginBottom: '1.875rem'
+    },
+    rightTitle: {
+        fontSize: '5rem',
+        lineHeight: '5rem'
+    },
+    rightTitleContainer: {
+        marginBottom: '1.875rem'
+    }
+});
 
 interface IHomeworkPageContainer {
     userType: UserTypes;
 }
 
 const HomeworkPageContainer: FC<IHomeworkPageContainer> = ({ userType }) => {
-    const menuList = useMemo(() => createMenuList(userType), [userType])
+    const classes = useStyles();
+    const menuList = useMemo(() => createMenuList(userType), [userType]);
     return (
         <NavigationMenu
             absolute
@@ -19,10 +50,46 @@ const HomeworkPageContainer: FC<IHomeworkPageContainer> = ({ userType }) => {
             LeftDrawerMenuComponent={<LeftDrawerMenuContent userType={userType} />}
         >
             <CardsGridContainer>
-                123
+                <ColumnsContainer
+                    topBorder={false}
+                    leftContent={
+                        <Grid container direction="column">
+                            <Grid container className={classes.filterContainer}>
+                                <FilterContainer
+                                    title="Homework"
+                                    sort={false}
+                                    filtersData={classesFiltersData}
+                                    initialFilterLabel="Filter"
+                                />
+                            </Grid>
+                            <Grid container direction="column" className={classes.counter}>
+                                <TitlePrimary>13</TitlePrimary>
+                                <Typography className={classes.counterTitle}>Incomplete</Typography>
+                            </Grid>
+                            <Grid container direction="column">
+                                <TitlePrimary>2</TitlePrimary>
+                                <Typography className={classes.counterTitle}>Overdue</Typography>
+                            </Grid>
+                        </Grid>
+                    }
+                    rightContent={
+                        <Grid container direction="column">
+                            <Grid container direction="column" className={classes.rightTitleContainer}>
+                                <Typography className={classes.rightTitle}>Total Assignments</Typography>
+                                <Typography className={classes.rightTitle}>15</Typography>
+                            </Grid>
+                            <Grid container>
+                                {homeworks.map((homework, index) => (
+                                    <HomeworkCard {...homework} key={index} />
+                                ))}
+                            </Grid>
+                        </Grid>
+                    }
+                />
             </CardsGridContainer>
+            <PageFooter />
         </NavigationMenu>
-    )
-}
+    );
+};
 
-export default HomeworkPageContainer
+export default HomeworkPageContainer;
---
+++ src/data/homework.ts
@@ -0,0 +1,45 @@
+import { HomeworkCardElement } from '../components/Cards/types';
+
+export const homeworks: HomeworkCardElement[] = [
+    {
+        title: 'Advanced linear algebra.',
+        subject: 'Maths',
+        dueDate: '11/08/20',
+        name: 'Mr J Cole',
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren…',
+    },
+    {
+        title: 'Advanced linear algebra.',
+        subject: 'Maths',
+        dueDate: '11/08/20',
+        name: 'Mr J Cole',
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren…',
+    },
+    {
+        title: 'Advanced linear algebra.',
+        subject: 'Maths',
+        dueDate: '11/08/20',
+        name: 'Mr J Cole',
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren…',
+        submitted: true,
+    },
+    {
+        title: 'Advanced linear algebra.',
+        subject: 'Maths',
+        dueDate: '11/08/20',
+        name: 'Mr J Cole',
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren…',
+    },
+    {
+        title: 'Advanced linear algebra.',
+        subject: 'Maths',
+        dueDate: '11/08/20',
+        name: 'Mr J Cole',
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren…',
+    },
+];
