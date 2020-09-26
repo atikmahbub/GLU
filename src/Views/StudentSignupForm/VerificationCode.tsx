4c4ce3d21f3b95c706d482c80ef93d5a8a01bf56
unknown <gyanendra@antino.io>
2020-09-26 22:23:04 +0530

    added the complete button

--- src/Views/StudentSignupForm/VerificationCode.tsx
+++ src/Views/StudentSignupForm/VerificationCode.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Typography } from '@material-ui/core';
 import OtpInput from 'react-otp-input';
 import { registerContext } from './Index';
+import OutlineButton from '../../components/Button/OutlineButton';
 
 interface props {
     onClick?: (value: string) => void;
@@ -36,6 +37,7 @@ const VerificationCode: React.FunctionComponent<props> = ({ onClick, changeNumbe
                 Didn’t receive a code <span onClick={resendPassword}>Resend </span> or
                 <span onClick={changeNumber}> Change number </span>
             </Typography>
+            <OutlineButton text="Complete" width="10rem" mt="5rem" btnClick={context.goNext} />
         </div>
     );
 };
